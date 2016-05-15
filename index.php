c8fb20b550cc7cc642474e40a25661227af6d44f
Cryzek <yogesh201996@gmail.com>
2016-05-15 12:18:24 +0530

    Navbar added

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<title>Placement Cell - IIITA</title>
-
+	<meta name="description" content="" />
+	<meta name="keywords" content="IIITA Placements,IIITA,Placements" />
 	<!--Import Google Icon Font-->
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!--Import materialize.css-->
@@ -13,18 +17,62 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+	
+	<div class="row">
+		<header class="col s12">
+			<div class="" id="nav-menu">
+				<a href="javascript:void(0)" class="menu-trigger waves-effect waves-dark">
+					<i class="material-icons small">menu</i>
+				</a>
+				<!-- Sidebar menu goes here -->
+				<div class="menu-wrapper white">
+					<div class="scroller">
+						<ul class="menu">
+							<li class="menu-list-items waves-effect waves-dark">
+								<i class="material-icons">business</i>
+								<a href="about.php">About IIITA</a>
+							</li>
+							<li class="menu-list-items waves-effect waves-dark">
+								<i class="material-icons">school</i>
+								<a href="academics.php">Academics</a>
+							</li>
+							<li class="menu-list-items waves-effect waves-dark">
+								<i class="material-icons">supervisor_account</i>
+								<a href="recruitment.php">For recruiters</a>
+							</li>
+							<li class="menu-list-items waves-effect waves-dark">
+								<i class="material-icons">supervisor_account</i>
+								<a href="students">Student Life</a>
+								<!-- <ul class="submenu">
+									<li><a class="icon icon-article">Articles</a></li>
+									<li><a class="icon icon-pictures">Images</a></li>
+									<li><a class="icon icon-videos">Videos</a></li>
+								</ul> -->
+							</li>
+							<li class="menu-list-items waves-effect waves-dark">
+								<i class="material-icons">place</i>
+								<a href="recruitment.php">Reach Us</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="" id="site-name"> 
+				<h5 class="center-align">IIITA Placement Division</h5>
+			</div>
+			<div class="" id="account">
+				<a href="javascript:void(0)" class="waves-effect waves-dark">
+					<i class="material-icons small">account_circle</i>
+				</a> 
+			</div>
+		</header>
+		<!-- <footer></footer> -->
+	</div>
+
 	<!--Import jQuery before materialize.js-->
-	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<!-- <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> -->
+	<script type="text/javascript" src="js/jquery-1.12.2.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
-	
-	<nav>
-	  <ul id="slide-out" class="side-nav">
-	    <li><a href="#!">First Sidebar Link</a></li>
-	    <li><a href="#!">Second Sidebar Link</a></li>
-	  </ul>
-	  <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="mdi-navigation-menu"></i></a>
-	</nav>
-
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,71 @@
 $(document).ready(function() {
-	$(".button-collapse").sideNav();
+
+	var $menuTrigger = $(".menu-trigger "),
+		$menuWrapper = $(".menu-wrapper"),
+		isMenuIconOpen = false,
+		isMenuOpen = false;
+
+	$menuTrigger.on("mouseover",function(e){
+		if(!isMenuIconOpen && !isMenuOpen){
+			$menuWrapper.addClass("open-part");
+			isMenuIconOpen = true;
+		}
+	});
+
+	$menuTrigger.on("click",function(e){
+		if(!isMenuIconOpen && !isMenuOpen){
+			$menuWrapper.addClass("open-part")
+			isMenuIconOpen = true;
+		}
+		else{
+			if(isMenuOpen){
+				$menuWrapper.removeClass("open-all");
+				isMenuOpen = false;
+			}
+			else{
+				$menuWrapper.addClass("open-all");
+				isMenuOpen = true;
+				$menuWrapper.removeClass("open-part");
+				isMenuIconOpen = false;
+			}
+		}
+		e.stopPropagation();
+	});
+
+	$menuWrapper.on("mouseover",function(){
+		if(!isMenuOpen){
+			$menuWrapper.addClass("open-all");
+			isMenuOpen = true;
+			$menuWrapper.removeClass("open-part");
+			isMenuIconOpen = false;
+		}
+	});
+
+	//Currently not needed.
+	// $menuWrapper.on("mouseout",function(){
+	// 	if(isMenuOpen){
+	// 		$menuWrapper.removeClass("open-all");
+	// 		isMenuOpen = false;
+	// 	}
+	// });
+
+	$(".menu-list-items a,.menu-list-items i").on("click",function(e){
+		e.stopPropagation();
+	})
+	
+	$(document).on("click",function(e){
+		if(isMenuOpen || isMenuIconOpen){
+			if(e.target.className == "menu-list-items" || e.target.className == "scroller" || e.target.className == "menu-trigger") return
+				console.log(e.target);
+			if(isMenuIconOpen){
+				$menuWrapper.removeClass("open-part");
+				isMenuIconOpen = false;
+			}
+			if(isMenuOpen){
+				$menuWrapper.removeClass("open-all");
+				isMenuOpen = false;
+			}
+		}
+	});
+
 })
